4754d056062e3f7fbb0794805aa9963381281302
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-05-09 13:29:16 +0000

    Implementação do exercício de enquetes na aula do dia 09/05.

--- projsala/enquete/views.py
+++ projsala/enquete/views.py
@@ -1,33 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-votos =[0,0,0,0]
-voto1=0
-voto2=0
-voto3=0
-voto4=0
+pergunta="Quanto é 2+2?"
+alternativas=["4","5","6","7"]
+votos=[0,0,0,0]
 def index(request):
-    pergunta="Quanto é 2+2?"
-    contexto={'pergunta':pergunta}
+    contexto={'pergunta':pergunta,"alternativas":alternativas,}
     return render(request, 'enquete/index.html',contexto)
 def votar(request):
     opcao=int(request.GET.get('opcao'))
-    global voto1
-    global voto2
-    global voto3
-    global voto4
-    if opcao == 1:
-        voto1 += 1
-    elif opcao == 2:
-        voto2 += 1
-    elif opcao == 3:
-        voto3 += 1
-    elif opcao == 4:
-        voto4 += 1
+    global votos
+    votos[opcao-1]+=1
+    resultado=zip(alternativas,votos)
     contexto = {
-        'voto1': voto1,
-        'voto2': voto2,
-        'voto3': voto3,
-        'voto4': voto4,
+        'resultado': resultado,
+        'pergunta': pergunta,
     }
     return render(request, 'enquete/resultado.html', contexto)
