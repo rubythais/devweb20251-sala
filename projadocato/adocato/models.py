4cf952a7d7ef27209822651daa0a933efbe15804
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-06-05 12:43:14 +0000

    Criação dos modelos Raça e Gato.

--- projadocato/adocato/models.py
+++ projadocato/adocato/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Raca(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+class Gato(models.Model):
+    nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1, choices=[('M', 'Macho'), ('F', 'Fêmea')])
+    cor = models.CharField(max_length=50)
+    dataNascimento = models.DateField()
+    descricao = models.TextField(blank=True, null=True)
+    disponivel = models.BooleanField(default=True)
+    raca = models.ForeignKey(Raca, on_delete=models.CASCADE, related_name='gatos')
